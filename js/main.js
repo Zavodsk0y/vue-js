3dff4f3480e1e0712065e72e23ffd1eeb638ded1
Zavodsk0y <zimniyvolk5@gmail.com>
2024-01-31 13:30:30 +0700

    Выполнена ПР №3. Объявили массив в дате и перебрали его с помощью v-for

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
     }
 })
 
