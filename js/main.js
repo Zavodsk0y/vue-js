64f322c6c45aca27ed78c81f4bd10b98124ce6b0
Zavodsk0y <zimniyvolk5@gmail.com>
2024-01-31 14:39:24 +0700

    Выполнена ПР №6, ознакомились с привязкой классов, добавлением стиля style, заменили названия цветов на цветные квадраты

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ let app = new Vue({
         altText: "A pair of socks",
 
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
+
+        inStock: false,
 
         inventory: 100,
 
